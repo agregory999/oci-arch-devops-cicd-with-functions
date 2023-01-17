60f61d6c3675b9d1bf905014246f2396043c2a5b
Andrew Gregory <agregory99@icloud.com>
2023-01-17 09:27:23 -0500

    Update providers.tf
    private key direct usage for TF Cloud

--- providers.tf
+++ providers.tf
@@ -30,7 +30,7 @@ provider "oci" {
 
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key = var.private_key
 }
 
 provider "oci" {
@@ -40,7 +40,7 @@ provider "oci" {
 
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key = var.private_key
 }
 
 provider "oci" {
@@ -50,5 +50,5 @@ provider "oci" {
 
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key = var.private_key
 }
